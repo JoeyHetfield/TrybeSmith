3c6d6c395aecc7f5e98041da919f832ed0a264cf
Joey Hetfield <joeydefenix@hotmail.com>
2023-04-24 14:57:28 -0300

    Controller da createProduct

---
+++ src/controllers/productController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { Product } from '../interfaces';
+import productService from '../services/productService';
+
+async function createProduct(req: Request, res: Response) {
+  const product = req.body as Product;
+  const { status, data } = await productService.createProduct(product);
+  res.status(status).json(data);
+}
+
+export default {
+  createProduct,
+};
